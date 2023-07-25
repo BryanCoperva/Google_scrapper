5f1f57c054dc95de499563e40a4a3f78f5a46d6b
BryanCoperva <ia_3@coperva.net>
2023-07-25 13:38:04 -0600

    Se agrega el script de los proxys y un text.ipynb para poder trabajar sobre de esos

--- proxy.py
+++ proxy.py
@@ -12,41 +12,39 @@ from tqdm import tqdm
 import re
 import csv
 
+# Se crean las opciones para conectarse a la página de los proxys
 chrome_options = Options()
 driver = webdriver.Chrome(options=chrome_options)
 start_url = 'http://free-proxy.cz/'
 driver.get(start_url)
 
-op = driver.find_element(By.XPATH, '//*[@id="frmsearchFilter-protocol-1"]')
-op.click()
-filter_proxy = driver.find_element(By.XPATH, '//*[@id="frmsearchFilter-send"]')
-filter_proxy.click()
+
+
+#op = driver.find_element(By.XPATH, '//*[@id="frmsearchFilter-protocol-1"]')
+#op.click()
+#filter_proxy = driver.find_element(By.XPATH, '//*[@id="frmsearchFilter-send"]')
+#filter_proxy.click()
 export = driver.find_element(By.ID, 'clickexport')
 export.click()
 
 # Extracción del html de la página
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-div_content = soup.find('div', {'id':'zkzk'}).get_text()
-
-#Limpieza
-clean = re.sub(r'\.$', '', div_content).strip()
+div_content = soup.find('div', {'id':'zkzk'}).decode_contents()
 
 #Utilizar split para extraer las direcciones IP y los puertos
 #proxies_list = [proxy.strip() for proxy in div_content.split('<br>') if proxy.strip()]
 
+# Utilizar expresión regular para extraer las direcciones IP y los puertos
 proxies_list = re.findall(r'\d+\.\d+\.\d+\.\d+:\d+', div_content)
-# Convertir la lista en un diccionario
-proxies_list = [{'http':f'http://{proxy}'} for proxy in proxies_list]
 
 # Tabla para guardar las direcciones IP y los puertos
 archivo_csv = 'proxies.csv'
 with open(archivo_csv, 'w', newline='') as file:
-    fieldname = ['http']
-    writer = csv.DictWriter(file, fieldnames=fieldname)
-    if file.tell() ==0:
-        writer.writeheader()
+    fieldnames = ['http']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
     for proxy in proxies_list:
-        writer.writerow(proxy)
+        writer.writerow({'http': f'http://{proxy}'})
 
 #print(div_content)
